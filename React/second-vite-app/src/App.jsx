1ae7a97641c3da6f7b1cc4fc8f3e768ee335a25d
coding-booth <vaibhavrma2017@gmail.com>
2024-11-13 20:34:25 +0530

    first commit

---
+++ React/second-vite-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import heading from './components/heading'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <heading/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, id tempore omnis eligendi esse veritatis ipsa nesciunt obcaecati maxime distinctio.</p>
+      <button>click here</button>
+    </>
+  )
+}
+
+export default App
---
+++ React/second-vite-app/src/components/heading.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import './heading.css';
+
+class heading extends Component{
+    render(){
+        return  <h1 className="heading">class vs function comp</h1>;
+    }
+}
+export default heading;
